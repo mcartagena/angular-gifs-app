a4ab94cfc9b29bf98180bdf92f4632b3d5d49d0b
Marcelo Cartagena <120571996+marcecarta@users.noreply.github.com>
2023-01-21 17:12:38 -0300

    working with service and pipes

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -7,9 +8,14 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class BusquedaComponent {
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService: GifsService) {}
+
   buscar(): void {
     const valor = this.txtBuscar.nativeElement.value;
-    console.log(valor);
+    if (valor.trim().length === 0) {
+      return;
+    }
+    this.gifsService.buscarGifs(valor);
     this.txtBuscar.nativeElement.value = '';
   }
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  constructor(private gifsService: GifsService) {}
+  get historial(): string[] {
+    return this.gifsService.historial;
+  }
+}
